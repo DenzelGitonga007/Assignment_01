420f1366a618176b676c7162fd6b57795402ebea
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-06-23 01:37:15 +0300

    The final update

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+function fetchName(){
+    let text_Name = document.getElementById("input_text").value;
+    document.getElementById("fetch_text").innerHTML = "Hello" + " " + text_Name + " " + "welcome to the bootcamp";
+
+}
